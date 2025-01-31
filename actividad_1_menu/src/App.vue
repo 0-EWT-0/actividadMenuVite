9ad2cb19be6c5cfcb755714d0b22488580feaca4
0-EWT-0 <iamewt@gmail.com>
2025-01-30 21:31:27 -0500

    base proyect

---
+++ actividad_1_menu/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper grid grid-cols-3 gap-4 align-center">
+      <nav v-for="link in ['Home', 'About', 'Dashboard']" :key="link">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ actividad_1_menu/src/views/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+   <h1 class="flex">Dashboard</h1>
+  </main>
+</template>
+
+<style scoped></style>
